69f9d7393787e2d562055dedadbedce701f612dc
Ajeenasics <ajeena.sics@gmail.com>
2024-10-07 13:31:17 +0530

    project initiated

---
+++ client/src/Components/User/UserLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../../Asserts/Styles/user.css"
+function UserLogin() {
+  return (
+    <div>
+      <div>jhjyghg</div>
+    </div>
+  )
+}
+
+export default UserLogin
