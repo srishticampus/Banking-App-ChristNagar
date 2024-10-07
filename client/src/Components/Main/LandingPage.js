50587d57e1e92642a5725abc75d5a759db110022
ajeena <ajeena.sics@gmail.com>
2024-10-07 17:04:29 +0530

    started

---
+++ client/src/Components/Main/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LandingNav from "./LandingNav";
+import LandingFooter from "./LandingFooter";
+
+function LandingPage() {
+  return (
+    <div>
+      <LandingNav />
+      <section>
+        <img src=""></img>
+      </section>
+      <LandingFooter />
+    </div>
+  );
+}
+
+export default LandingPage;
