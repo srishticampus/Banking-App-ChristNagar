bb6b58f1d22f77813b27adb6936269ff68bf6756
Ajeenasics <ajeena.sics@gmail.com>
2024-11-01 14:50:49 +0530

    sprint1 completed

--- client/src/apis/imgURL.js
+++ client/src/apis/imgURL.js
@@ -1,6 +1,6 @@
-const imgurl= "http://localhost:4045";
+// const imgurl= "http://localhost:4045";
 
-   // const imgurl= "http://hybrid.srishticampus.in/4045",
+   const imgurl= "http://hybrid.srishticampus.in/4045"
 
 
 export default imgurl;
