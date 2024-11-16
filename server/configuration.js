9c337a7096bb66d20b25f5ea1f400402689f86d4
Ajeenasics <ajeena.sics@gmail.com>
2024-11-16 09:45:40 +0530

    new build

--- server/configuration.js
+++ server/configuration.js
@@ -2,7 +2,7 @@
 const config = {
     development: {
         serverUrl: 'http://hybrid.srishticampus.in/bank_app/reset-password/',
-        // localUrl: 'http://localhost:3000/bank_app/reset-password/',
+        localUrl: 'http://localhost:3000/bank_app/reset-password/',
 
     },
   
