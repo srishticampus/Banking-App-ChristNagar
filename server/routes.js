0ace44bec998959a0e1da95f5e7220546e76ae5d
Ajeenasics <ajeena.sics@gmail.com>
2024-12-02 17:48:31 +0530

    backend

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,7 @@ const router=express.Router()
 
 router.post('/addmanger',manager.upload,manager.adminAddManager)
 router.get('/viewallmangers',manager.viewManagers)
-router.get('/view_a_manger/:managerid',manager.viewManagerById)
+router.get('/view_a_manager/:managerid',manager.viewManagerById)
 router.post('/editManagerById/:managerid',manager.upload,manager.editManagerById)
 router.post('/deactivate_a_manger/:id',manager.deActivateManagerById)
 router.post('/activate_a_manger/:id',manager.activateManagerById)
